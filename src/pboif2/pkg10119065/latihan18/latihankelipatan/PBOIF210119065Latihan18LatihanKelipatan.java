6650ec27d8002a70d7efc1265f90008c1ba415fa
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-30 22:20:01 +0700

    upload

---
+++ src/pboif2/pkg10119065/latihan18/latihankelipatan/PBOIF210119065Latihan18LatihanKelipatan.java
@@ -0,0 +1,22 @@
+package pboif2.pkg10119065.latihan18.latihankelipatan;
+
+ /*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini menampilkan kelipatan bilangan 
+ */
+import java.util.Scanner;
+public class PBOIF210119065Latihan18LatihanKelipatan {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);        
+        double bilangan = 3.5;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(bilangan * i);
+        }
+        
+       System.out.println("(Developed by : Fachriansyah Muhammad Nur Ihsan)");  
+    }
+    
+}
